ab2fcd7c468cf57057d61cd40ee1ff15449c6336
AntonioVitalic <48534043+AntonioVitalic@users.noreply.github.com>
2023-08-09 19:26:54 -0400

    Antonio edita usuario.py por segunda vez

--- usuario.py
+++ usuario.py
@@ -8,8 +8,8 @@ class Usuario:
     def agregarTarea(self, tarea):
         self.tareas.append(tarea)
     def listarTareas(self):
-    	for tarea in self.tareas:
-            if tarea.estaLista():
-            	print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-            	print(f"[ ] {tarea.obtenerNombre()}" )
+       for tarea in self.tareas:
+           if tarea.estaLista():
+               print(f"La tarea {tarea.obtenerNombre()} está lista")
+           else: <<<<<<< Elimina esta
+               print(f"La tarea {tarea.obtenerNombre()} no está lista")
