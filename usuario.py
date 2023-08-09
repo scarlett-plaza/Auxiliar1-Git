5ead231bbb923713ae3b61b2e967bc411ad0b99e
Computador Scar <scarlett.plaza@ug.uchile.cl>
2023-08-09 19:24:40 -0400

    Elimina else de usuario.py

--- usuario.py
+++ usuario.py
@@ -11,5 +11,3 @@ class Usuario:
     	for tarea in self.tareas:
             if tarea.estaLista():
             	print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-            	print(f"[ ] {tarea.obtenerNombre()}" )
